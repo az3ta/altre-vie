e4b8e40ef6a4b7cc86ff11b3209c5256b93c8cad
az3ta <24527778+az3ta@users.noreply.github.com>
2023-11-04 12:02:29 +0100

    intro message + custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blu: "#386DC0",
+        rosso: "#CA2B2B",
+      },
+      fontFamily: {
+        sans: ["Jost", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
