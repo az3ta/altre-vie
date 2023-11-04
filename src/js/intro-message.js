d4f3032ed7cacac9a184371268c73de5de3b2559
az3ta <24527778+az3ta@users.noreply.github.com>
2023-11-04 16:04:35 +0100

    try components 1

---
+++ src/js/intro-message.js
@@ -0,0 +1,16 @@
+const template = document.createElement('template')
+template.innerHTML = `
+  <h1 class="w-full h-24 bg-red-300">
+    Hello, World!
+  </h1>
+`
+  
+
+class IntroMessage extends HTMLElement {
+  constructor() {
+    super();
+    this.innerHTML = }
+}
+
+customElements.define("intro-message", IntroMessage);
+
