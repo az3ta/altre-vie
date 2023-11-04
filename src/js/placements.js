017e7388a6ffc17229ec85425452bbafc07881b0
Yyyyaaaannnnoooo <yannpatrickmartins@gmail.com>
2023-11-04 15:59:18 +0100

    add animations and audio

---
+++ src/js/placements.js
@@ -0,0 +1,114 @@
+const placements = [
+  {
+    audio: './media/audio/AQUILA.mp3',
+    animation: './media/animations_webm/AQUILA.webm',
+  },
+  {
+    audio: './media/audio/CITTA.mp3',
+    animation: './media/animations_webm/CITTA.webm',
+  },
+  {
+    audio: './media/audio/COCCINELLE.mp3',
+    animation: './media/animations_webm/COCCINELLE.webm',
+  },
+  {
+    audio: './media/audio/CORALLO.mp3',
+    animation: './media/animations_webm/CORALLO.webm',
+  },
+  {
+    audio: './media/audio/DAMERINO.mp3',
+    animation: './media/animations_webm/DAMERINO.webm',
+  },
+  {
+    audio: './media/audio/FOGLIA.mp3',
+    animation: './media/animations_webm/FOGLIA.webm',
+  },
+  {
+    audio: './media/audio/INSETTO.mp3',
+    animation: './media/animations_webm/INSETTO.webm',
+  },
+  {
+    audio: './media/audio/LEC.mp3',
+    animation: './media/animations_webm/LEC.webm',
+  },
+  {
+    audio: './media/audio/LETTER_EXPLOSION.mp3',
+    animation: './media/animations_webm/EXPLOSION.webm',
+  },
+  {
+    audio: './media/audio/LETTER_SCATTERING.mp3',
+    animation: './media/animations_webm/SCATTERING.webm',
+  },
+  {
+    audio: './media/audio/LETTERA_DAMORE.mp3',
+    animation: './media/animations_webm/AMORE.webm',
+  },
+  {
+    audio: './media/audio/LETTERINA.mp3',
+    animation: './media/animations_webm/LETTERINA.webm',
+  },
+  {
+    audio: './media/audio/LUMACA.mp3',
+    animation: './media/animations_webm/LUMACA.webm',
+  },
+  {
+    audio: './media/audio/MANI.mp3',
+    animation: './media/animations_webm/MANI.webm',
+  },
+  {
+    audio: './media/audio/MATRIX.mp3',
+    animation: './media/animations_webm/MATRIX.webm',
+  },
+  {
+    audio: './media/audio/PALLINA.mp3',
+    animation: './media/animations_webm/PALLINA.webm',
+  },
+  {
+    audio: './media/audio/PAPERA.mp3',
+    animation: './media/animations_webm/PAPERA.webm',
+  },
+  {
+    audio: './media/audio/PAVONE.mp3',
+    animation: './media/animations_webm/PAVONE.webm',
+  },
+  {
+    audio: './media/audio/PIETRA.mp3',
+    animation: './media/animations_webm/PIETRA.webm',
+  },
+  {
+    audio: './media/audio/PRATO.mp3',
+    animation: './media/animations_webm/PRATO.webm',
+  },
+  {
+    audio: './media/audio/PRUGNE.mp3',
+    animation: './media/animations_webm/PRUGNE.webm',
+  },
+  {
+    audio: './media/audio/RAGAZZA.mp3',
+    animation: './media/animations_webm/RAGAZZA.webm',
+  },
+  {
+    audio: './media/audio/RIGHELLO.mp3',
+    animation: './media/animations_webm/RIGHELLO.webm',
+  },
+  {
+    audio: './media/audio/SCALE.mp3',
+    animation: './media/animations_webm/SCALE.webm',
+  },
+  {
+    audio: './media/audio/SCRITTURA_IMMAGINARIA.mp3',
+    animation: './media/animations_webm/SCRITTURA_IMMAGINARIA.webm',
+  },
+  {
+    audio: './media/audio/SIGNOR_OSTRICA.mp3',
+    animation: './media/animations_webm/SIGNOR_OSTRICA.webm',
+  },
+  {
+    audio: './media/audio/STELLA_MARINA.mp3',
+    animation: './media/animations_webm/STELLA_MARINA.webm',
+  },
+  {
+    audio: './media/audio/UCCELLINO.mp3',
+    animation: './media/animations_webm/UCCELLINO.webm',
+  },
+]
