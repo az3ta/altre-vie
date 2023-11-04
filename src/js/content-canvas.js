e2a2fee357403fc270ec1f2b7dd7131a4d44f8ec
az3ta <24527778+az3ta@users.noreply.github.com>
2023-11-04 17:17:12 +0100

    map container

--- src/js/content-canvas.js
+++ src/js/content-canvas.js
@@ -1,6 +1,8 @@
 export class ContentCanvas extends HTMLElement {
   connectedCallback() {
     this.innerHTML = /*html*/ `
+    
+
       <section class="flex items-center justify-center w-full h-full">
         <picture>
           <img src="./img/test-animation.webp" alt="">
