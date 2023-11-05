4bb359ec5daabd058b1cb509331d66b2f64befc8
az3ta <24527778+az3ta@users.noreply.github.com>
2023-11-05 13:08:22 +0100

    fix about bg when active

--- src/js/site-header.js
+++ src/js/site-header.js
@@ -8,16 +8,16 @@ export class SiteHeader extends HTMLElement {
         <menu class="flex gap-2 md:gap-4 lg:gap-8">
           <li 
             class="px-5 md:px-8 hover:bg-blu bg-white/90 hover:text-white p-2.5 rounded-full border-2 border-solid border-blu text-blu text-2xl sm:text-3xl md:text-4xl"
-            :class="about ? 'bg-blu text-blu' : ''"
+            :class="about ? 'bg-blu/100 text-white' : ''"
           >
             <button 
               @click="about = ! about" 
               type="button" 
               role="button"
             >
-              <span class="block md:hidden">A</span>
+              <span class="block md:hidden" x-text="about ? 'X' : 'A'"></span>
               <span 
-                class="hidden md:block">ABOUT</span>
+                class="hidden md:block" x-text="about ? 'CLOSE' : 'ABOUT'"></span>
             </button>
           </li>
           <li class="px-5 md:px-8 py-2.5 bg-white/90 rounded-full border-2 border-solid border-rosso hover:bg-rosso hover:text-white text-rosso text-2xl sm:text-3xl  md:text-4xl">
